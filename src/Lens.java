4beaaa9ee90de2ac6886504af2a37489f55404b7
JamaicaPerol <116412365+JamaicaPerol@users.noreply.github.com>
2022-11-19 21:28:52 +0800

    Create Lens.java

---
+++ src/Lens.java
@@ -0,0 +1,9 @@
+public class Lens
+{
+	public String testLens(){
+		return "EF-S mount lens is lightweight and delivered superb images for DSLR with APS C sensor.";
+	}
+	public String donetestLens(){
+		return "The lens testing is done.";
+	}
+}
